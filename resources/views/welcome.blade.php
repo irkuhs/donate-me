16b002c308724e774f0eceb74bad6bb931abf83a
irkuhs <shukriahmad56@gmail.com>
2024-07-13 22:47:33 +0800

    feat: create form req

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,133 +18,100 @@
     <link href="{{ asset('metronic/dist/assets/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
     <!--end::Global Stylesheets Bundle-->
 </head>
-<!--end::Head-->
-<!--begin::Body-->
 
-<body id="kt_body" class="app-blank app-blank bgi-size-cover bgi-position-center bgi-no-repeat">
-    <!--begin::Root-->
-    <div class="d-flex flex-column flex-root" id="kt_app_root">
-        <!--begin::Page bg image-->
-        <style>
-            body {
-                /* background-image: url({{ asset('staff/src/media/auth/bg6.jpg') }}); */
-                background-color: #000000;
-            }
+    <body id="kt_body" class="app-blank app-blank bgi-size-cover bgi-position-center bgi-no-repeat">
+        <div class="d-flex flex-column flex-root" id="kt_app_root">
+            <style>
+                #kt_app_root {
+                    background-color: #777777 !important; /* Ensure root div also has black background */
+                    height: 100vh; /* Make sure it takes full height */
+                }
 
-            [data-theme="dark"] body {
-                background-color: #000000;
-            }
+                .centered-container {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                }
 
-            .centered-container {
-                display: flex;
-                justify-content: center;
-                align-items: center;
-            }
+                .centered-image {
+                    width: 70%;
+                    height: 70%;
+                    border: 2px solid rgb(255, 255, 255);
+                }
 
-            .centered-image {
-                width: 70%;
-                height: 70%;
-                border: 2px solid black;
-            }
-        </style>
-        <!--end::Page bg image-->
-        <!--begin::Authentication - Sign-in -->
-        <div class="d-flex flex-column flex-column-fluid flex-lg-row">
-            <!--begin::Aside-->
-            <div class="d-flex flex-center w-lg-50 pt-15 pt-lg-0 px-10">
-                <!--begin::Aside-->
-                <div class="d-flex flex-center flex-lg-start flex-column">
-                    <!--begin::Logo-->
-                    <a href="#" class="mb-6 centered-container">
-                        <img alt="Logo" src="{{ asset('metronic/src/media/irkuhs/irkuhs-2.png') }}" class="centered-image" />
-                    </a>
-                    <!--end::Logo-->
-                </div>
-                <!--begin::Aside-->
-            </div>
-            <!--begin::Aside-->
-            <!--begin::Body-->
-            <div class="d-flex flex-center w-lg-50 p-10">
-                <!--begin::Card-->
-                <div class="card rounded-3 w-md-550px">
-                    <!--begin::Card body-->
-                    <div class="card-body p-10 p-lg-20">
-                        <!--begin::Form-->
-                        <form class="form w-100" novalidate="novalidate" id="kt_sign_in_form"
-                            action="" method="POST">
-                            @csrf
-                            <!--begin::Heading-->
-                            <div class="text-center mb-11">
-                                <!--begin::Title-->
-                                <h1 class="text-dark fw-bolder mb-3">Donate-Me</h1>
-                                <!--end::Title-->
-                                <!--begin::Subtitle-->
-                                <div class="text-gray-500 fw-semibold fs-6">This platform is created by IRKUHS.
-                                </div>
-                                <!--end::Subtitle=-->
-                            </div>
-                            <!--begin::Heading-->
-                            <!--begin::Input group=-->
-                            <div class="fv-row mb-8">
-                                <!--begin::Email-->
-                                <input type="email" placeholder="Email" name="email" autocomplete="off"
-                                    class="form-control bg-transparent @error('email') is-invalid @enderror"
-                                    value="{{ old('email') }}" autocomplete="email" required autofocus />
-                                <!--end::Email-->
-                            </div>
-                            <!--end::Input group=-->
-                            <div class="fv-row mb-3">
-                                <!--begin::Password-->
-                                <input type="password" placeholder="Password" name="password" autocomplete="off"
-                                    class="form-control bg-transparent @error('password') is-invalid @enderror"
-                                    name="password" required autocomplete="current-password" />
-                                <!--end::Password-->
-                            </div>
-                            <!--end::Input group=-->
-                            <!--begin::Wrapper-->
-                            <div class="d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8">
-                                <div></div>
-                                <!--begin::Link-->
-                                <a href="" class="link-primary">Forgot Password ?</a>
-                                <!--end::Link-->
-                            </div>
-                            <!--end::Wrapper-->
-                            <!--begin::Submit button-->
-                            <div class="d-grid mb-10">
-                                <button type="submit" class="btn btn-primary">
-                                    <span class="indicator-label">Sign In</span>
-                                </button>
-                            </div>
-                            <!--end::Submit button-->
-                            <!--begin::Sign up-->
-                            <!--<div class="text-gray-500 text-center fw-semibold fs-6">Not a Member yet?-->
-                            <!--<a href="../../demo1/dist/authentication/layouts/creative/sign-up.html" class="link-primary">Sign up</a></div>-->
-                            <!--end::Sign up-->
-                        </form>
-                        <!--end::Form-->
+                .d-flex {
+                    display: flex;
+                }
+
+                .justify-content-between {
+                    justify-content: space-between;
+                }
+
+                .btn-secondary {
+                    flex: 1;
+                    margin: 0 5px;
+                }
+
+                .custom-card {
+                    border: 2px solid #d86b11;
+                    border-radius: 10px;
+                    box-shadow: 0 4px 15px rgba(0, 0, 0, 0.1);
+                }
+            </style>
+            <div class="d-flex flex-column flex-column-fluid flex-lg-row">
+                <div class="d-flex flex-center w-lg-50 pt-15 pt-lg-0 px-10">
+                    <div class="d-flex flex-center flex-lg-start flex-column">
+                        <a href="#" class="mb-6 centered-container">
+                            <img alt="Logo" src="{{ asset('metronic/src/media/irkuhs/irkuhs-2.png') }}" class="centered-image" />
+                        </a>
                     </div>
-                    <!--end::Card body-->
                 </div>
-                <!--end::Card-->
-            </div>
-            <!--end::Body-->
-        </div>
-        <!--end::Authentication - Sign-in-->
-    </div>
-    <!--end::Root-->
-    <!--begin::Javascript-->
-    <script>
-        var hostUrl = "assets/";
-    </script>
-    <!--begin::Global Javascript Bundle(mandatory for all pages)-->
-    <script src="{{ asset('metronic/dist/assets/plugins/global/plugins.bundle.js') }}"></script>
-    <script src="{{ asset('metronic/dist/assets/js/scripts.bundle.js') }}"></script>
-    <!--end::Global Javascript Bundle-->
-    <!--begin::Custom Javascript(used for this page only)-->
-    <script src="{{ asset('metronic/dist/assets/js/custom/authentication/sign-in/general.js') }}"></script>
-    <!--end::Custom Javascript-->
-    <!--end::Javascript-->
-</body>
-<!--end::Body-->
+                <div class="d-flex flex-center w-lg-50 p-10">
+                    <div class="card rounded-3 w-md-550px custom-card">
+                        <div class="card-body p-10 p-lg-20">
+                            @if(session('success'))
+                                <div class="alert alert-success mb-4" role="alert">
+                                    {{ session('success') }}
+                                </div>
+                            @endif
+                            <form class="form w-100" novalidate="novalidate" id="kt_sign_in_form" action="{{ route('donate') }}" method="POST">
+                                @csrf
+                                <div class="text-center mb-11">
+                                    <h1 class="text-dark fw-bolder mb-3">Donate-Me</h1>
+                                    <div class="text-gray-500 fw-semibold fs-6">This platform is created by IRKUHS.</div>
+                                </div>
+                                <div class="fv-row mb-10">
+                                    <label class="form-label fs-6 fw-bolder text-dark">Amount (RM)</label>
+                                    <input class="form-control form-control-lg" type="number" name="amount" id="amount" />
+                                </div>
+                                <div class="d-flex mb-10 justify-content-between">
+                                    <button type="button" class="btn btn-secondary" onclick="setAmount(3.00)">RM 3</button>
+                                    <button type="button" class="btn btn-secondary" onclick="setAmount(5.00)">RM 5</button>
+                                    <button type="button" class="btn btn-secondary" onclick="setAmount(10.00)">RM 10</button>
+                                    <button type="button" class="btn btn-secondary" onclick="setAmount(15.00)">RM 15</button>
+                                </div>
+                                <div class="d-grid mb-10">
+                                    <button type="submit" class="btn btn-primary">
+                                        <span class="indicator-label">Donate</span>
+                                    </button>
+                                </div>
+                            </form>
 
+                            <script>
+                                function setAmount(value) {
+                                    document.getElementById('amount').value = value;
+                                }
+                            </script>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script>
+            var hostUrl = "assets/";
+        </script>
+        <script src="{{ asset('metronic/dist/assets/plugins/global/plugins.bundle.js') }}"></script>
+        <script src="{{ asset('metronic/dist/assets/js/scripts.bundle.js') }}"></script>
+        <script src="{{ asset('metronic/dist/assets/js/custom/authentication/sign-in/general.js') }}"></script>
+    </body>
 </html>
